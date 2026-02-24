0d47d4f4024cc1516527248d58fea6a74132522a
Prajwal M V <mvprajwal546@gmail.com>
2026-02-24 20:22:55 +0530

    Minimal Basic Authentication

---
+++ src/main/java/com/codesnippet/weather_service/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.codesnippet.weather_service.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    //Minimal Basic Authentication
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
